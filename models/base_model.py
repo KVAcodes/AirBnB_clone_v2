8d8a5d0a4af4e8f4c3e5aec50f37437bc6b9ad6b
KVAcodes <kadiriavictor@gmail.com>
2023-09-12 21:03:19 +0000

    changed basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
@@ -30,6 +28,7 @@ class BaseModel:
                     self.__dict__[k] = datetime.strptime(v, tform)
                 else:
                     self.__dict__[k] = v
+        self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
