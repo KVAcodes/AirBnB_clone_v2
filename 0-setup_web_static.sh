dd5fe0da7f42121e3873de288a5b8d9eee706ae6
KVAcodes <kadiriavictor@gmail.com>
2023-09-15 01:18:36 +0000

    testing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,36 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Sets up my web servers for the deployment of web_static.
 
-apt-get update
-apt-get install -y nginx
+# Installs Nginx if not already installed
+if ! dpkg -l | grep -q "nginx"
+then
+sudo apt-get -y update
+sudo apt-get -y install nginx
+ufw allow 'Nginx HTTP'
 
+# Creates the necessary directories
 mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+mkdir /data/web_static/shared/
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Creates index.html file.
+html_content="<html>
+\t<head>
+\t</head>
+\t<body>
+\t\tHolberton School
+\t/body>
+</html>"
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+echo -e "$html_content" > /data/web_static/releases/test/index.html
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
+# Creates a symbolic link, deletes and recreates it if it exists.
+if [ -L /data/web_static/current ]
+then
+    rm /data/web_static/current
+fi
+ln -s /data/web_static/releases/test/ /data/web_static/current
 
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
+# Gives ownership of /data folder to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
 
-service nginx restart
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,7 +2,6 @@
 """Defines unnittests for models/engine/file_storage.py."""
 import os
 import json
-import pep8
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
@@ -77,12 +76,6 @@ class TestFileStorage(unittest.TestCase):
         del cls.city
         del cls.amenity
         del cls.review
-
-    def test_pep8_FileStorage(self):
-        """Test pep8 styling."""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_docstrings(self):
         """Check for docstrings."""
