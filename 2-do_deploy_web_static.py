a8626019747ebef9e21d170deb00b6c394361b7d
KVAcodes <kadiriavictor@gmail.com>
2023-09-16 14:42:10 +0000

    testing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,21 +23,31 @@ def do_deploy(archive_path):
     if put_result.failed:
         return False
 
-    archive_name = archive_path.split('/')[-1]
-    archive_name_wt_ext = archive_name.split('.')[0]
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    run_result = run(
-        f"mkdir -p /data/web_static/releases/{archive_name_wt_ext}\n"
-        f"tar -xzf /tmp/{archive_name} -C "
-        f"/data/web_static/releases/{archive_name_wt_ext}\n"
-        f"mv /data/web_static/releases/{archive_name_wt_ext}/web_static/* "
-        f"/data/web_static/releases/{archive_name_wt_ext}/\n"
-        f"rm -rf /data/web_static/releases/{archive_name_wt_ext}/web_static\n"
-        f"rm /tmp/{archive_name}\n"
-        f"rm -rf /data/web_static/current\n"
-        f"ln -s /data/web_static/releases/{archive_name_wt_ext}/ "
-        f"/data/web_static/current\n"
-    )
-    if run_result.failed:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
         return False
     return True
