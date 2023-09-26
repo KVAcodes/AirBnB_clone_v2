db30b0317b210ac3b30cd6c003314e82690000e8
KVAcodes <kadiriavictor@gmail.com>
2023-09-26 20:25:14 +0000

    added task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application.
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """ """
+    return f"C {' '.join(text.split('_'))}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text='is_cool'):
+    """ """
+    return f"Python {' '.join(text.split('_'))}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ """
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """ """
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run()
