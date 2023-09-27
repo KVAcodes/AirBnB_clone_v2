a171aa229203e926711714e04f3b908a94ba7f02
KVAcodes <kadiriavictor@gmail.com>
2023-09-27 21:31:36 +0000

    added task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application.
+"""
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove(exception=None):
+    """ """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def list_state():
+    state_list = storage.all("State").values()
+    nameId_list = [(state.id, state.name) for state in
+                   state_list]
+    nameId_list = sorted(nameId_list, key=lambda x: x[1])
+    print(f"-->: {nameId_list}")
+    return render_template("7-states_list.html", nameId_list=nameId_list)
+
+
+if __name__ == "__main__":
+    app.run()
