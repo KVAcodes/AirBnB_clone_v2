45d8a90909c0da42608a45f2be3ce178be2f5f8b
KVAcodes <kadiriavictor@gmail.com>
2023-09-28 00:14:58 +0000

    tst

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,7 +2,6 @@
 """This script starts a Flask web application.
 """
 from models import storage
-from os import getenv
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -13,20 +12,11 @@ def remove(exception=None):
     storage.close()
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def list_state():
-    state_list = storage.all("State").values()
-    sorted_state_list = sorted(state_list, key=lambda state: state.name)
-    state_city_list = []
-    for state in sorted_state_list:
-        city_list = state.cities
-        if city_list:
-            state_city_list.append(city_list)
-        else:
-            state_city_list.append([])
-
-    return render_template("8-cities_by_states.html", states=sorted_state_list,
-                           cities=state_city_list, length=len(state_list))
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == "__main__":
